f6a3ec69001affa52b2bbcd78c6db17097de23de
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-04 00:28:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,7 @@ class PigLatinize
   attr_reader :text
 
   def initialize(text)
-    @text = text.downcase
+    @text = text
   end
 
   def piglatinize
@@ -11,13 +11,23 @@ class PigLatinize
     arr.collect do |word|
       if word.start_with?("a", "e", "i", "o", "u")
         word += "ay"
+        word.downcase.capitalize! if capital_letter?(word)
       else
         word_arr = word.split(/^([^aeiou]+)/).drop(1)
         front = word_arr.shift
         word_arr << front
         word_arr << "ay"
         word_arr.join
+        word.downcase.capitalize! if capital_letter?(word)
       end
     end.join(" ")
+  end
+
+  def capital_letter?(word)
+    word =~ /[A-Z]/ ? true : false
+  end
+
+  def punctuation?(word)
+    word.ends_with?(".", "," ":", ";", "!", "?") ? true : false
   end
 end
