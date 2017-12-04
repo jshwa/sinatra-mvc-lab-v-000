7674ac77dbb8acaab5e6f3ab4d04091c5f985814
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-04 01:40:03 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,13 +6,15 @@ class PigLatinizer
     arr.collect do |word|
       punctuation?(word) ? punct = word.slice!(/\W/) : punct = nil
 
-      if word !~ /^[AEIOUaeiou]/
+      if word =~ /^[AEIOUaeiou]/
+        word += "way"
+      else
         word_arr = word.split(/^([^aeiou]+)/).drop(1)
         word_arr << word_arr.shift
         word = word_arr.join
+        word += "ay"
       end
 
-        word += "ay"
         word += punct if punct != nil
         capital_letter?(word) ? word.capitalize! : word
 
