f5289a2d9dcd443063e8fc5f2e93e7528039ef94
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-04 02:01:25 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,7 +17,7 @@ class PigLatinizer
         word += "ay"
       end
 
-        # word += punct if punct != nil
+        word += punct if punct != nil
         # capital_letter?(word) ? word.capitalize! : word
 
     end.join(" ")
